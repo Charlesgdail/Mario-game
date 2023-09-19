526eeed8b3832b922832921c8a175a5860042ecb
Charlesgdail <137565677+Charlesgdail@users.noreply.github.com>
2023-09-19 15:26:24 -0400

    Add files via upload

---
+++ Mario Game/mario.js
@@ -0,0 +1,14 @@
+const { Client, Account, Databases, ID, Query} = Appwrite
+const projectId = '6505c8abc9fe6d6d6af1'
+const databaseId = ''
+const collectionId = ''
+
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(projectId)
+
+const account = new Account(client)
+
+function register(event){
+    account.create
+}
